b76d38e15e56d2db512a31e8b965565982ab1428
leolau <leolau2012@gmail.com>
2014-09-30 20:25:41 +0800

    ajax使用实例-瀑布流
    ajax使用实例-瀑布流

---
+++ ajax实例-瀑布流/getPic.php
@@ -0,0 +1,11 @@
+<?php 
+
+header('content-type:text/html;charset="utf-8"');
+
+$cpage =isset($_GET['cpage'])?$_GET['cpage']:1;
+$url ='http://www.wookmark.com/api/json/popular?page='.$cpage;
+$content = file_get_contents($url);
+$content = iconv('gbk','utf-8',$content);
+
+echo $content;
+?>
